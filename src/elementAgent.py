b14c75b324317a8bcd2759597c441584edc38c1e
Javid yousaf <javidyousaf@outlook.com>
2020-05-13 18:17:59 +0100

    finishing touches

--- src/elementAgent.py
+++ src/elementAgent.py
@@ -4,47 +4,79 @@ Assignment: 2 - White Star Line
 Author: Javid Yousaf
 Student id: 201385963
 Date: 15/05/2020
+version: 1.0
 
 Description: Class to capture and calculate information about each element 
 """
 
+
 class AreaElement:
+    '''
+    Captures the combined information from lidar and radar data at a single
+    point in the area grid.
+    '''
+
     def __init__(self, x, y, radar_value, lidar_value):
-        self._y = x # x coord of element in the area matrix
-        self._x = y # y coord of element in the area matrix
-        self._radar_value = radar_value # radar value at the coord.
-        self._lidar_value = lidar_value # radar value at the coord.
+        '''
+        To store data about the area element.
+        The class is initailsed by providing the x, y coords and
+        the radar and lidar vlaues at these coords.
+        '''
+        self._y = x
+        self._x = y
+        self._radar_value = radar_value
+        self._lidar_value = lidar_value 
 
+    ''' Getters and Setters to set and access class properties safely '''
 
-    # Getters and Setters for class properties
     def get_x(self):
+        ''' get x coord of element in the area matrix '''
         return self._x
 
     def set_x(self, value):
+        ''' set x coord of element in the area matrix '''
         self._x = value
 
     def get_y(self):
+        ''' get y coord of element in the area matrix '''
         return self._y
 
     def set_y(self, value):
+        ''' set y coord of element in the area matrix '''
         self._y = value
 
     def get_radar_value(self):
+        ''' get radar value of element in the area matrix '''
         return self._radar_value
 
     def set_radar_value(self, value):
+        ''' set radar value of element in the area matrix '''
         self._radar_value = value
 
     def get_lidar_value(self):
+        ''' get lidar value of element in the area matrix '''
         return self._lidar_value
 
     def set_lidar_value(self, value):
+        ''' set lidar value of element in the area matrix '''
         self._lidar_value = value
 
+    '''
+    Calculated functions to provide additional data for the iceberg
+    '''
+
     def is_ice(self):
+        '''
+        Determine if the element is ice.
+        Returns True if the element value is 100 or more.
+        Returns False if the element value is leass than 100.
+         '''
         return self._radar_value > 99
 
     def calculate_height_as(self):
+        '''
+        Calculates the elements height in metres above sea level.
+         '''
         if (self._lidar_value > 0):
             return self._lidar_value / 10
         else:
--- src/icebergAgent.py
+++ src/icebergAgent.py
@@ -4,6 +4,7 @@ Assignment: 2 - White Star Line
 Author: Javid Yousaf
 Student id: 201385963
 Date: 15/05/2020
+version: 1.0
 
 Description: Class to store and calculate information about an iceberg.
 """
@@ -11,29 +12,54 @@ Description: Class to store and calculate information about an iceberg.
 
 class Iceberg:
     def __init__(self, volume_as, mass_as):
-        self._volume_as = volume_as
-        self._mass_as = mass_as
+        '''
+        To store data about the iceberg.
+        The class is initailsed by providing the mass and volume above sea level
+        '''
+        self._volume_as = volume_as  # volume above sea level
+        self._mass_as = mass_as  # mass above sea level
 
-    # Getters and Setters for class properties
+    '''
+    Getters and Setters to set and access class properties safely
+    '''
+
     def get_volume_as(self):
+        ''' get volume above sea level '''
         return self._volume_as
 
     def set_volume_as(self, value):
+        ''' set volume above sea level '''
         self._volume_as = value
 
     def get_mass_as(self):
+        ''' get mass above sea level '''
         return self._mass_as
 
     def set_mass_as(self, value):
+        ''' set mass above sea level '''
         self._mass_as = value
 
+    '''
+    Calculated functions to provide additional data for the iceberg
+    '''
 
-    # calculated functions
     def get_total_volume(self):
+        '''
+        Calculates the total volume of the iceberg on the assumption that
+        90% of the volume is below sea level.
+        '''
         return self._volume_as * 10
 
     def get_total_mass(self):
+        '''
+        Calculates the total mass of the iceberg on the assumption that
+        90% of the mass is below sea level.
+        '''
         return self._mass_as * 10
 
     def is_towable(self):
-        self.get_total_mass() < 36000000
+        '''
+        Returns True if the iceberg mass is less than 36 million kg's.
+        Returns False if the iceberg mass is 36 million kg's or more.
+        '''
+        return self.get_total_mass() < 36000000
